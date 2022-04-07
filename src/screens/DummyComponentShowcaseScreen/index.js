5fd93d446361451cc06f2d4ef2800a6791b3cfcd
Zhuocheng Lin <zhuochenglin@zhuochengs-mbp.myfiosgateway.com>
2022-04-07 13:34:04 -0400

    WP-64 Add route for dummy component screen

---
+++ src/screens/DummyComponentShowcaseScreen/index.js
@@ -0,0 +1,8 @@
+const DummyComponentShowcaseScreen = () => {
+    return (
+        <div>
+            Create component files under the "./DummyComponentShowcaseScreen" directory and show those components here.
+        </div>
+    )
+};
+export default DummyComponentShowcaseScreen;
