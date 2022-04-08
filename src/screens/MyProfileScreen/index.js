5270ec68cccff5d4a976b12f7eb174735072458e
Zhuocheng Lin <zhuochenglin@zhuochengs-mbp.myfiosgateway.com>
2022-04-07 22:55:23 -0400

    WP-75 Initial commit

--- src/screens/MyProfileScreen/index.js
+++ src/screens/MyProfileScreen/index.js
@@ -1,7 +1,24 @@
+import {useSelector} from "react-redux";
+import {getUserState, isLoggedIn} from "../../redux/selectors";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
 const MyProfileScreen = () => {
+    const loggedIn = useSelector(isLoggedIn);
+    const loggedInUser = useSelector(getUserState);
+    const navigate = useNavigate();
+    const checkLogin = () => {
+        if (!loggedIn) {
+            alert("Please login first!")
+            navigate("/login");
+        }
+    }
+    useEffect(checkLogin);
     return (
         <div>
-            My Profile
+            My Profile:
+            <div>UserID: {loggedInUser.userId}</div>
+            <div>Username: {loggedInUser.username}</div>
         </div>
     )
 };
--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,7 +1,19 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {getUserId} from "../../redux/selectors";
+import {useEffect} from "react";
 
 const ProfileScreen = () => {
     let params = useParams();
+    const navigate = useNavigate();
+    const profileOwnerId = params.uid;
+    const loggedInUserId = useSelector(getUserId);
+    const isThisMyProfile = () => {
+        if (loggedInUserId === profileOwnerId) {
+            navigate("/profile")
+        }
+    }
+    useEffect(isThisMyProfile, [loggedInUserId, navigate, profileOwnerId]);
     return (
         <div>
             Profile of {params.uid ? params.uid : "me"}
