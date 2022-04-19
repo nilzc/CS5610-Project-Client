1f7b3a35eee14df96686909404cc8ae279687540
Pavan Choudhari <pavanwebdevcs5610@gmail.com>
2022-04-19 14:51:25 -0400

    Changes to the layout and added extra click handlers

--- src/components/MovieReviewItem/index.js
+++ src/components/MovieReviewItem/index.js
@@ -4,6 +4,7 @@ import * as reviewServices from "../../services/reviewServices";
 import * as errorServices from "../../services/errorServices";
 import {MY} from "../../services/constants";
 import MovieItem from "../MovieItem";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const MovieReviewItem = ({
                              review = {
@@ -12,10 +13,15 @@ const MovieReviewItem = ({
                          }) => {
     const loggedInUserId = useSelector(getUserId);
     const isMyReview = review.postedBy._id === loggedInUserId;
+    const location = useLocation();
     const deleteReview = () => {
         reviewServices.deleteReview(MY, review._id)
             .then((res) => refresh())
             .catch(errorServices.alertError);
+    }
+    const navigate = useNavigate();
+    const onClickNavigate = (movie) => {
+        navigate('/movies/'+movie.id);
     }
     return (
         <div className={"row d-flex flex-row justify-content-between bg-light list-group-item p-0"}>
@@ -27,11 +33,24 @@ const MovieReviewItem = ({
             {/*}*/}
             <div className="col-12 card border-0 bg-light">
                 <div className="card-body d-flex justify-content-between">
-                    <div>
-                        <h5 className="card-title p-1">User: {review.postedBy.username}</h5>
+                {location.pathname === '/profile/s/reviews' && <div className="row" onClick={() => onClickNavigate(review.movie)}>
+                        <div className="col-4">
+                            <img src={`${review.movie.poster_path ? `${process.env.REACT_APP_MOVIE_BASE_URL}/w342/${review.movie.poster_path}` : ""}`} className="img-fluid" alt="Poster Not Found" />
+                        </div>
+                        <div className="col-8">
+                        <h5 className="card-title p-1">{review.movie.original_title}</h5> 
                         <h6 className="card-subtitle text-muted p-1 ">Rating: {review.rating}</h6>
                         <p className="card-text p-1">{review.review}</p>
-                    </div>
+                        </div>
+                    </div>}
+                    {location.pathname !== '/profile/s/reviews' && <div>
+                        <div>
+                        <h5 className="card-title p-1">Written by <span className="text-success fw-bold"><u>{review.postedBy.username}</u>
+                        </span> on {new Date(review.createdOn).toString().split(' ').slice(1,3).join(' ')}, {new Date(review.createdOn).getFullYear()}</h5>
+                        <p className="card-text p-1">{review.review}</p>
+                        <h6 className="card-subtitle text-muted p-1 ">Rating: {review.rating}</h6>
+                        </div>
+                    </div>}
                     <div>
                         {
                             isMyReview &&
